cd4e732146218e2f9c7baaced9f20226f8cb2299
John Danquah-Boateng <yjbdanquah@gmail.com>
2026-04-23 19:43:25 +0000

    fixes'

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as log from 'loglevel';
 import * as process from 'node:process';
-import {json} from "express";
 
 async function bootstrap() {
   log.setLevel('debug');
